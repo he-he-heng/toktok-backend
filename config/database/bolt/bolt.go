f4b3e3c3afc38698637c420f8792af2972219cfa
fullgukbap <2hanjunbum6@gmail.com>
2024-06-19 11:33:58 +0900

    refactor: bolt 데이터베이스 bucket 생성구문 추가

--- config/database/bolt/bolt.go
+++ config/database/bolt/bolt.go
@@ -17,7 +17,23 @@ type Database struct {
 }
 
 func NewDatabase(c *config.Database) (*bolt.DB, error) {
-	return bolt.Open(c.Bolt.Path, fs.FileMode(c.Bolt.Mode), &bolt.Options{Timeout: 1 * time.Second})
+	db, err := bolt.Open(c.Bolt.Path, fs.FileMode(c.Bolt.Mode), &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return nil, err
+	}
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(StatusBucket))
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, err
 }
 
 func (d *Database) Close() error {
