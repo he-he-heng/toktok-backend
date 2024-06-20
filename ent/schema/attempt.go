eddec4554e561b8d15df038d39c0b35a2e757bf5
fullgukbap <2hanjunbum6@gmail.com>
2024-06-20 09:24:26 +0900

    feat: 데이터베이스 추가

---
+++ ent/schema/attempt.go
@@ -0,0 +1,27 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// Attempt holds the schema definition for the Attempt entity.
+type Attempt struct {
+	ent.Schema
+}
+
+// Fields of the Attempt.
+func (Attempt) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("cnt").Default(0),
+		field.Bool("break").Default(false),
+		field.Time("timestamp").Default(time.Now),
+	}
+}
+
+// Edges of the Attempt.
+func (Attempt) Edges() []ent.Edge {
+	return nil
+}
--- pnumstate/pnumstate_repo.go
+++
@@ -1,8 +0,0 @@
-package pnumstate
-
-import "github.com/he-he-heng/toktok-backend/model"
-
-type PnumStateRepo interface {
-	Create(pnum string, pnumState *model.PnumStatus) (*model.PnumStatus, error)
-	ReadByPnum(pnum string) (*model.PnumStatus, error)
-}
