ba27d31d90e6a1b3b9866d4230bacb3dfba624b8
fullgukbap <2hanjunbum6@gmail.com>
2024-06-24 20:10:58 +0900

    feat: config 서비스 구현

---
+++ internal/adapter/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"os"
+
+	"github.com/pelletier/go-toml/v2"
+)
+
+type Config struct {
+	Database struct {
+		User     string
+		Password string
+		Host     string
+		Port     string
+		Database string
+	}
+
+	Token struct {
+		AccessDuration  string
+		RefreshDuration string
+	}
+}
+
+func New(path string) (*Config, error) {
+	config := new(Config)
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	err = toml.NewDecoder(file).Decode(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
