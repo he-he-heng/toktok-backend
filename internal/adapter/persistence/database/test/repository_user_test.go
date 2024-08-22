333f6d6a710e4c24be8992c3d946957ea18e75d3
한준범 <fullgukbap@hanjunbeom-ui-MacBookAir.local>
2024-08-22 12:07:58 +0900

    test: getUser (repository)

--- internal/adapter/persistence/database/test/repository_user_test.go
+++ internal/adapter/persistence/database/test/repository_user_test.go
@@ -17,7 +17,7 @@ func init() {
 }
 
 func TestCreateUser(t *testing.T) {
-	defer provider.Get().TerminateContainer(context.Background())
+	defer provider.Get().TerminateContainer(provider.MyContext)
 
 	testCases := []struct {
 		user   *domain.User
@@ -73,8 +73,60 @@ func TestCreateUser(t *testing.T) {
 			tc.expect.DeletedAt = got.DeletedAt
 
 			if !reflect.DeepEqual(tc.expect, got) {
-				tsub.Errorf("tc.expect and got don't have each other. got=%+v\nexpect=%+v\n", got, tc.expect)
+				tsub.Errorf("\ntc.expect and got don't have each other.\ngot=%+v\nexpect=%+v\n", got, tc.expect)
 			}
 		})
 	}
+}
+
+func TestGetUser(t *testing.T) {
+	defer provider.Get().TerminateContainer(provider.MyContext)
+
+	ctx := context.Background()
+	// database seedling
+	userRepo.CreateUser(ctx, &domain.User{
+		UID:      "socketdokkdj3",
+		Password: "fjfji3lski3",
+	})
+
+	testCases := []struct {
+		selectId int
+		expect   *domain.User
+	}{
+		{
+			selectId: 1,
+			expect: &domain.User{
+				ID:       1,
+				UID:      "socketdokkdj3",
+				Password: "fjfji3lski3",
+				Role:     domain.UserRoleUser,
+				BanState: domain.UserBanStateUnBan,
+			},
+		},
+	}
+
+	for i, tc := range testCases {
+		name := fmt.Sprintf("#%d GetUser", i)
+		t.Run(name, func(tsub *testing.T) {
+			got, err := userRepo.GetUser(ctx, tc.selectId)
+			if err != nil {
+				tsub.Errorf("failed to get user: %v", err)
+			}
+
+			tc.expect.Email = got.Email
+			tc.expect.CreatedAt = got.CreatedAt
+			tc.expect.UpdatedAt = got.UpdatedAt
+			tc.expect.DeletedAt = got.DeletedAt
+
+			if !reflect.DeepEqual(tc.expect, got) {
+				tsub.Errorf("\ntc.expect and got don't have each other.\ngot=%+v\nexpect=%+v\n", got, tc.expect)
+			}
+		})
+	}
+
+}
+
+func TestListUser(t *testing.T) {
+	defer provider.Get().TerminateContainer(provider.MyContext)
+
 }
